8801f3472422558fffbfd502003ae87ec80a8148
Andyliwr <andyliwr@outlook.com>
2017-11-23 19:57:07 +0800

    2017/11/23 更新

--- koa/config/index.js
+++ koa/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-	port: 3000,
+	port: 5000,
 	mongo_url: 'mongodb://localhost:27017/mbook',
 	wx_appid: 'wxc7bb707dc3f735ed', // 小程序appid
 	wx_secret: 'd2b60beed687a23c9ec56dc3d8374216', // 小程序秘钥
