f4e362c9ca3ea7d329a5fb4281caffcb50aaa60c
DiKang Li <121960425@qq.com>
2018-09-24 23:39:11 +0800

    完成好友助力的一些接口

--- api/friendHelp.js
+++ api/friendHelp.js
@@ -1,19 +1,114 @@
-import { FriendHelp, Book, User } from '../models'
+import { FriendHelp, FriendHelpBook, User, Secret } from '../models'
+import { checkUserToken, checkAdminToken } from '../utils'
+import shortid from 'shortid'
 
 export default function(router) {
+  // 开始好友助力
   router.post('/api/friend_help', async (ctx, next) => {
-    // todo
+    let userid = await checkUserToken(ctx, next)
+    if (!userid) {
+      return false
+    }
+    let { fhbid, source } = ctx.request.body
+    if (!fhbid || !source) {
+      ctx.body = { ok: false, msg: '参数错误' }
+    }
+    // 检查fhbid是否存在
+    let friendHelpBook = await FriendHelpBook.findById(fhbid)
+    if (!friendHelpBook) {
+      ctx.body = { ok: false, msg: '找不到此好友助力书籍' }
+      return false
+    }
+    // 检查来源的正确性
+    let rightSourceArr = ['banner', 'book_detail', 'reader']
+    if (rightSourceArr.indexOf(source) < 0) {
+      ctx.body = { ok: false, msg: '来源错误' }
+      return false
+    }
+    // 检测当前用户是否已经对该书籍存在分享
+    let tmpFriendHelp = await FriendHelp.findOne({ userid, fhbid }, '_id fhcode')
+    if (tmpFriendHelp) {
+      ctx.body = { ok: true, msg: '已存在该书籍的好友助力', fhcode: tmpFriendHelp.fhcode }
+    } else {
+      // 创建friendHelp
+      let friendHelp = await FriendHelp.create({
+        fhbid: await FriendHelp.transId(fhbid),
+        userid: await FriendHelp.transId(userid),
+        fhcode: shortid.generate(),
+        records: [],
+        source,
+        success: false,
+        create_time: new Date()
+      })
+      ctx.body = { ok: true, msg: '创建好友助力成功', fhcode: friendHelp.fhcode }
+    }
   })
 
-  router.get('/api/friend_help', async (ctx, next) => {
-    // todo
-  })
-
-  router.put('/api/friend_help/:id', async (ctx, next) => {
-    // todo
-  })
-
-  router.delete('/api/friend_help/:id', async (ctx, next) => {
-    // todo
+  // 好友接受助力分享
+  router.get('/api/friend_help/accept', async (ctx, next) => {
+    let userid = await checkUserToken(ctx, next)
+    if (!userid) {
+      return false
+    }
+    let fhcode = ctx.request.query.fhcode
+    // 校验fhcode合法性，存在并且未过期
+    let friendHelp = await FriendHelp.findOne({ fhcode })
+    if (!friendHelp) {
+      ctx.body = { ok: false, msg: '参数错误' }
+      return false
+    }
+    if (friendHelp.success) {
+      ctx.body = { ok: false, msg: '好友助力已经完成' }
+      return false
+    }
+    // 检验是否已经接受助力了
+    let hasAccept = friendHelp.records.some(item => {
+      return item.uid.toString() === userid
+    })
+    if (hasAccept) {
+      ctx.body = { ok: false, msg: '您已经接受了该好友助力' }
+      return false
+    }
+    // 是否超时，或者达标
+    let friendHelpBook = await FriendHelpBook.findById(friendHelp.fhbid)
+    let now = new Date()
+    if (friendHelpBook.limit_time && now.getTime() - friendHelp.create_time.getTime() > friendHelpBook.limit_time * 24 * 60 * 60 * 1000) {
+      ctx.body = { ok: false, msg: '好友助力超时' }
+      return false
+    }
+    if (friendHelpBook.need_num && friendHelp.records.length >= friendHelpBook.need_num) {
+      ctx.body = { ok: false, msg: '好友助力已完成' }
+      return false
+    }
+    // 为好友助力添加记录
+    let currentUser = await User.findById(userid)
+    let data = {
+      uid: currentUser.id,
+      name: currentUser.username,
+      avatar: currentUser.avatar,
+      time: new Date()
+    }
+    // 判断是否已完成
+    let success = false
+    if (friendHelpBook.need_num && friendHelp.records.length + 1 === friendHelpBook.need_num) {
+      success = true
+    }
+    // 开始更新
+    let updateResult = await FriendHelp.update({ fhcode }, { $addToSet: { records: data }, $set: { success } })
+    if (updateResult.ok == 1 && updateResult.nModified == 1) {
+      if (success) {
+        // 发放完成好友助力的奖励，自动为其解锁该书籍
+        console.log('好友助力完成，自动为其解锁该书籍')
+        await Secret.create({
+          userid: await FriendHelpBook.transId(userid),
+          bookid: await FriendHelpBook.transId(friendHelpBook.bookid),
+          active: true,
+          create_time: new Date()
+        })
+      }
+      ctx.body = { ok: true, msg: '接受好友助力成功' }
+    } else {
+      ctx.body = { ok: false, msg: '接受好友助力失败' }
+    }
   })
 }
--- api/friendHelpBook.js
+++ api/friendHelpBook.js
@@ -1,5 +1,5 @@
-import { FriendHelpBook, Book } from '../models'
-import { checkAdminToken } from '../utils'
+import { FriendHelpBook, Book, FriendHelp } from '../models'
+import { checkAdminToken, checkUserToken } from '../utils'
 
 export default function(router) {
   router.post('/api/friend_help_book', async (ctx, next) => {
@@ -31,11 +31,13 @@ export default function(router) {
       ctx.body = { ok: false, msg: '当前书籍已经是好友助力书籍' }
       return false
     }
+    let count = await FriendHelpBook.count()
     // 创建助力信息
     friendHelpBook = await FriendHelpBook.create({
       bookid: await FriendHelpBook.transId(bookid),
       need_num: need_num,
       limit_time: limit_time,
+      index: count + 1,
       create_time: new Date()
     })
     ctx.body = { ok: true, msg: '创建助力书籍成功', data: friendHelpBook }
@@ -69,7 +71,7 @@ export default function(router) {
         path: 'bookid',
         select: '_id name img_url author'
       })
-      .sort({ create_time: -1 })
+      .sort({ index: 1, create_time: -1 })
       .skip((page - 1) * limit)
       .limit(limit)
     ctx.body = { ok: true, msg: '获取好友助力书籍成功', total: allFriendHelpBookNum, list: friendHelpBooks }
@@ -82,7 +84,7 @@ export default function(router) {
     }
     let { need_num, limit_time } = ctx.request.body
     let friendHelpBookid = ctx.params.id
-    // 检查bookid是否存在
+    // 检查friendHelpBookid是否存在
     if (!friendHelpBookid) {
       ctx.body = { ok: false, msg: '参数错误' }
       return false
@@ -120,13 +122,13 @@ export default function(router) {
       return false
     }
     let friendHelpBookid = ctx.params.id
-    // 检查gooid
+    // 检查friendHelpBookid是否存在
     if (!friendHelpBookid) {
       ctx.body = { ok: false, msg: '参数错误' }
       return false
     }
     // 检查friendHelpBookid是否正确
-    let friendHelpBook = await FriendHelpBook.findById(friendHelpBookid, 'id')
+    let friendHelpBook = await FriendHelpBook.findById(friendHelpBookid, 'id index')
     if (!friendHelpBook) {
       ctx.body = { ok: false, msg: '好友助力书籍不存在' }
       return false
@@ -134,9 +136,163 @@ export default function(router) {
     // 删除好友助力书籍
     let deleTeResult = await FriendHelpBook.remove({ _id: friendHelpBookid })
     if (deleTeResult.result.ok === 1) {
-      ctx.body = { ok: true, msg: '删除好友助力书籍成功' }
+      // 更新数据库中的index
+      let needUpdateArr = await FriendHelpBook.find({ index: { $gt: friendHelpBook.index } }, '_id')
+      let updateArr = []
+      for (let i = 0; i < needUpdateArr.length; i++) {
+        updateArr.push(
+          new Promise((resolve, reject) => {
+            FriendHelpBook.update({ _id: needUpdateArr[i].id }, { $set: { index: friendHelpBook.index + i } }, (err, res) => {
+              if (err) {
+                reject(err)
+                return false
+              } else {
+                resolve(true)
+              }
+            })
+          })
+        )
+      }
+      return new Promise((resolve, reject) => {
+        Promise.all(updateArr)
+          .then(res => {
+            // 更新完成
+            ctx.body = { ok: true, msg: '删除好友助力书籍成功' }
+            resolve(true)
+          })
+          .catch(err => {
+            ctx.body = { ok: false, msg: '更新助力书籍顺序值失败' }
+            resolve(true)
+          })
+      })
     } else {
       ctx.body = { ok: false, msg: '删除好友助力书籍失败', data: deleTeResult.result }
+    }
+  })
+
+  // 后台助力书籍置顶接口
+  router.get('/api/friend_help_book/:id/top', async (ctx, next) => {
+    let friendHelpBookid = ctx.params.id
+    // 检查friendHelpBookid是否存在
+    if (!friendHelpBookid) {
+      ctx.body = { ok: false, msg: '参数错误' }
+      return false
+    }
+    // 检查friendHelpBookid是否正确
+    let friendHelpBook = await FriendHelpBook.findById(friendHelpBookid, 'id index')
+    if (!friendHelpBook) {
+      ctx.body = { ok: false, msg: '好友助力书籍不存在' }
+      return false
+    }
+    // 开始更新
+    let needUpdateArr = await FriendHelpBook.find({ index: { $lt: friendHelpBook.index } }, '_id index')
+    let updateArr = []
+    for (let i = 0; i < needUpdateArr.length; i++) {
+      updateArr.push(
+        new Promise((resolve, reject) => {
+          FriendHelpBook.update({ _id: needUpdateArr[i].id }, { $set: { index: i + 2 } }, (err, res) => {
+            if (err) {
+              reject(err)
+              return false
+            } else {
+              resolve(true)
+            }
+          })
+        })
+      )
+    }
+    updateArr.push(
+      new Promise((resolve, reject) => {
+        FriendHelpBook.update({ _id: friendHelpBookid }, { $set: { index: 1 } }, (err, res) => {
+          if (err) {
+            reject(err)
+            return false
+          } else {
+            resolve(true)
+          }
+        })
+      })
+    )
+    return new Promise((resolve, reject) => {
+      Promise.all(updateArr)
+        .then(res => {
+          // 更新完成
+          ctx.body = { ok: true, msg: '助力书籍置顶成功' }
+          resolve(true)
+        })
+        .catch(err => {
+          ctx.body = { ok: false, msg: '助力书籍置顶失败' }
+          resolve(true)
+        })
+    })
+  })
+
+  // 小程序端获取好友助力书籍列表
+  router.get('/api/friend_help_book/list', async (ctx, next) => {
+    // 判断小程序端是否登录
+    let userid = ''
+    if (ctx.header.authorization && ctx.header.authorization.split(' ').length === 2) {
+      userid = await checkUserToken(ctx, next)
+    }
+    let { page, limit } = ctx.request.query
+    if (page) {
+      page = parseInt(page)
+      if (page < 1) {
+        page = 1
+      }
+    } else {
+      page = 1
+    }
+    if (limit) {
+      limit = parseInt(limit)
+      if (limit < 1) {
+        limit = 10
+      }
+    } else {
+      limit = 10
+    }
+    let allFriendHelpBookNum = await FriendHelpBook.count()
+    let friendHelpBooks = await FriendHelpBook.find()
+      .populate({
+        path: 'bookid',
+        select: '_id name img_url author'
+      })
+      .sort({ index: 1, create_time: -1 })
+      .skip((page - 1) * limit)
+      .limit(limit)
+    // 如果用户已经登录，同时要返回每本书的助力状态
+    if (userid) {
+      let findArr = []
+      let newArray = JSON.parse(JSON.stringify(friendHelpBooks))
+      for (let i = 0; i < friendHelpBooks.length; i++) {
+        findArr.push(
+          new Promise((resolve, reject) => {
+            FriendHelp.findOne({ userid, fhbid: friendHelpBooks[i].id }, 'id success', (err, res) => {
+              if (err) {
+                reject(err)
+                return false
+              }
+              if (res) {
+                newArray[i].success = res.success
+              }
+              resolve(true)
+            })
+          })
+        )
+      }
+      return new Promise((resolve, reject) => {
+        Promise.all(findArr)
+          .then(res => {
+            ctx.body = { ok: true, msg: '获取好友助力书籍成功', total: allFriendHelpBookNum, list: newArray }
+            resolve(true)
+          })
+          .catch(err => {
+            ctx.body = { ok: true, msg: '获取好友助力书籍成功', total: allFriendHelpBookNum, list: newArray }
+            resolve(true)
+          })
+      })
+    } else {
+      ctx.body = { ok: true, msg: '获取好友助力书籍成功', total: allFriendHelpBookNum, list: friendHelpBooks }
     }
   })
 }
