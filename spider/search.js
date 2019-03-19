c3ddf4830ef7e7caf7fbcb83bb5a40986879f12a
lidikang <lidikang@myhexin.com>
2019-03-19 22:11:07 +0800

    爬虫

--- spider/search.js
+++ spider/search.js
@@ -9,24 +9,26 @@ requestProxy(request);
 async function doGetRequest(url) {
   console.log(userAgent())
   request
-    .get('url')
+    .get('http://www.77xsw.la/')
     .set({ 'User-Agent': userAgent() })
     .timeout({ response: 5000, deadline: 60000 })
-    .proxy(ip)
+    .proxy('111.177.190.31:9999')
     .end(async (err, res) => {
+			console.log('TCL: doGetRequest -> err, res', err, res)
       // 处理数据
     });
 }
 
 async function searchQianQianFaction(keyword='') {
   return new Promise((resolve, reject) => {
-
+    await doGetRequest()
   })
 }
 
 async function searchThirdPartFaction(keyword='') {
   // 搜索千千小说网
-  let result1 = searchQianQianFaction();
+  let result1 = await doGetRequest();
+	console.log('TCL: searchThirdPartFaction -> result1', result1)
 }
 
 export { searchThirdPartFaction }
