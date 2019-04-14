1337ce55249641f34ff17811bb63a5fe484a8b68
Andyliwr <andyliwr@outlook.com>
2019-04-14 23:14:11 +0800

    修改schedule.js

--- bin/shedule.js
+++ bin/shedule.js
@@ -17,7 +17,9 @@ async function run() {
   // 每隔10分钟刷新redis中的代理ip地址
   schedule.scheduleJob('*/10 * * * *', getProxyIpAddress)
   // 每天凌晨执行更新
-  schedule.scheduleJob('0 0 1 * * *', updateBook)
+  schedule.scheduleJob('0 0 3 * * *', async function() {
+    await updateBook()
+  })
 }
 
 module.exports = {
